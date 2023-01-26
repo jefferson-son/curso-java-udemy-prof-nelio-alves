f1edb445c41cadf3209f2f667cffcd0fa7e0d704
Jefferson <jeffersonferreira.son.dev@gmail.com>
2023-01-26 18:28:50 -0300

    Finalização do programa

--- 20_resolvendo_segundo_problema_exemplo/src/application/Program.java
+++ 20_resolvendo_segundo_problema_exemplo/src/application/Program.java
@@ -21,7 +21,24 @@ public class Program {
 		System.out.print("Quantity: ");
 		product.quantity = sc.nextInt();
 		
-		System.out.println(product);
+		System.out.println();
+		System.out.println("Product data: " + product);
+		
+		System.out.println();
+		System.out.print("Enter the number od products to be add in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Update data: " + product);
+		
+		System.out.println();
+		System.out.print("Enter the number od products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Update data: " + product);
 		
 		sc.close();
 
--- 20_resolvendo_segundo_problema_exemplo/src/entities/Product.java
+++ 20_resolvendo_segundo_problema_exemplo/src/entities/Product.java
@@ -26,9 +26,6 @@ public class Product {
 			+ " units, "
 			+ "Total: $ "
 			+ String.format("%.2f", totalValueInStock());
-				
-				
-				
-				
+									
 	}
 }
