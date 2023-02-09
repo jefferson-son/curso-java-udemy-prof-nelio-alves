7c88f7b067e65c2f28ab1c74feccd6110661d1c0
Jefferson <jeffersonferreira.son.dev@gmail.com>
2023-02-09 13:27:15 -0300

    Adionado exercício resolvido sobre polimosfismo

---
+++ 43_exercicio_resolvido_polimorfismo/src/application/Program.java
@@ -0,0 +1,58 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+import entities.OutsourcedEmployee;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+		List<Employee> employees = new ArrayList<>();
+
+		System.out.print("Enter the number of employees: ");
+		int n = scanner.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			System.out.println("Employee #" + (i + 1) + " data: ");
+			System.out.print("Outsourced (y/n)? ");
+			char outsourced = scanner.next().charAt(0);
+			System.out.print("Name: ");
+			scanner.nextLine();
+			String name = scanner.nextLine();
+			System.out.print("Hours: ");
+			int hours = scanner.nextInt();
+			System.out.print("Value per hour: ");
+			double valuePerHour = scanner.nextDouble();
+
+			if (outsourced == 'y') {
+				System.out.print("Additional charge: ");
+				double additionalCharge = scanner.nextDouble();
+
+				employees.add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+			} else {
+				employees.add(new Employee(name, hours, valuePerHour));
+
+			}
+
+		}
+
+		System.out.println();
+
+		System.out.println("PAYMENTS: ");
+		for (Employee emp : employees) {
+			String name = emp.getName();
+			double payment = emp.payment();
+
+			System.out.printf("%s - $ %.2f%n", name, payment);
+		}
+
+		scanner.close();
+	}
+
+}
