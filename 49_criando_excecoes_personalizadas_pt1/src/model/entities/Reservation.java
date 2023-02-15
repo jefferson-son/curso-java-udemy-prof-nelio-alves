be24d313c579bf6e8c31467dd0ab553d0537287b
Jefferson <jeffersonferreira.son.dev@gmail.com>
2023-02-15 09:50:53 -0300

    Aula sobre exceções personalizadas parte 1

---
+++ 49_criando_excecoes_personalizadas_pt1/src/model/entities/Reservation.java
@@ -0,0 +1,62 @@
+package model.entities;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Reservation {
+	private Integer roomNumber;
+	private LocalDate checkIn;
+	private LocalDate checkOut;
+	
+	private static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+	public Reservation() {
+	}
+
+	public Reservation(Integer roomNumber, LocalDate checkIn, LocalDate checkOut) {
+		this.roomNumber = roomNumber;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public LocalDate getCheckIn() {
+		return checkIn;
+	}
+
+	public LocalDate getCheckOut() {
+		return checkOut;
+	}
+
+	public Duration duration() {
+		Duration diff = Duration.between(checkOut, checkIn);
+		return diff;
+	}
+	
+	public void updateDates(LocalDate checkIn, LocalDate checkOut) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	@Override
+	public String toString() {
+		return "Romm"
+				+ roomNumber
+				+", check-in: "
+				+ dtf.format(checkIn)
+				+ ", checkOut: "
+				+ dtf.format(checkOut)
+				+ ", "
+				+ duration()
+				+ " nights";
+			
+	}
+}
